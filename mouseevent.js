1d046bd96e1758b21a1e16588a455e897b1dd6ef
shivani111-ux <shinynagelli@gmail.com>
2025-04-23 11:43:45 +0530

    Update mouseevent.js

--- mouseevent.js
+++ mouseevent.js
@@ -1,37 +1,44 @@
 /*Write a program to display a message using onMouse event handlers*/
 import React, { useState } from 'react';
 
-function App() {
-  const [message, setMessage] = useState('');
+const boxStyle = {
+  width: '200px',
+  height: '100px',
+  backgroundColor: 'lightblue',
+  lineHeight: '100px',
+  margin: '50px auto',
+  borderRadius: '10px',
+  fontSize: '18px',
+  textAlign: 'center',
+  transition: 'background-color 0.3s'
+};
 
-  const handleMouseOver = () => {
-    setMessage('You hovered over the box......');
-  };
+const messageStyle = {
+  textAlign: 'center',
+  fontSize: '20px',
+  color: '#333'
+};
 
-  const handleMouseOut = () => {
-    setMessage('');
+function MouseEventExample() {
+  const [message, setMessage] = useState('Waiting for interaction...');
+
+  const handleMouseEvent = (isHovering) => {
+    setMessage(isHovering ? 'Mouse is over the box!' : 'Mouse left the box.');
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div>
       <div
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        style={{
-          width: '200px',
-          height: '100px',
-          margin: '0 auto',
-          backgroundColor: '#f0f0f0',
-          border: '2px solid #333',
-          lineHeight: '100px',
-          cursor: 'pointer'
-        }}
+        style={boxStyle}
+        onMouseEnter={() => handleMouseEvent(true)}
+        onMouseLeave={() => handleMouseEvent(false)}
       >
-        Hover Over Me
+        Hover over me!
       </div>
-      <p>{message}</p>
+      <p style={messageStyle}>{message}</p>
     </div>
   );
 }
 
-export default App;
+export default MouseEventExample;
+
